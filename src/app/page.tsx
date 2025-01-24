4632668e3f06029973166a0e39494c0ee76d8450
erum-waris <erumwaris93@gmail.com>
2025-01-24 15:24:14 +0500

    Next-Quiz-App

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
        
         <p className="md:text-2xl text-xl mb-6 mt-4 rounded-lg text-white bg-blue-950 mx-7 p-5">Next.js is a powerful React framework that enables developers to build full-stack web applications with features like server-side rendering and static site generation. It simplifies the development process by handling configurations such as bundling and compiling, allowing you to focus on building your application. With Next.js, you can create fast, dynamic, and interactive React applications efficiently.</p>
         <Link href="/basic-quiz">
-          <button className="bg-blue-600 text-white text-md md:text-xl py-3 px-6 rounded-lg hover:bg-gray-700">
+          <button className="bg-blue-600 mb-24 text-white text-md md:text-xl py-3 px-6 rounded-lg hover:bg-gray-700">
             Attempt Quiz
           </button>
         </Link>
